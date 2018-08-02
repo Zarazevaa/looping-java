838d174a72af1017b62604fe3ff73e47d7ef642c
ZEVA <ZEVA@LAPTOP-HTFI5LEL>
2018-08-02 08:17:50 +0700

    For While Do-While

---
+++ src/J08/contoh2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J08;
+
+/**
+ *
+ * @author ZEVA
+ */
+public class contoh2 {
+    public static void main(String[] args) {
+        // FOR
+        int x;
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.print(x + " ");
+        }
+        
+        // WHILE
+        x = 1;
+        while (x <= 10)
+        {
+            System.out.println(x + " ");
+            x++;
+        }
+        
+        // DO WHILE
+        x = 1;
+        do 
+        {
+            System.out.println(x + " ");
+            x++;
+        }
+        while (x <= 10);
+                
+    }
+    
+}
